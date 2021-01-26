086714ca94e1c221569a4aaf650c240c672d0576
Ronald Pitts <ronpitts81@gmail.com>
2021-01-26 18:28:22 -0500

    win function

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-const userScore = 0; 
-const compScore = 0; 
+let userScore = 0; 
+let compScore = 0; 
 
 const scoreboardDiv = document.querySelector(".score-board"); 
 const userScoreEl = document.getElementById("user-score");
 const compScoreEl = document.getElementById("comp-score");
 
 
-const resultDiv = document.querySelector(".result"); 
+const resultDiv = document.querySelector(".result > p"); 
 
 const rockDiv = document.getElementById("roc"); 
 const paperDiv = document.getElementById("pape");
@@ -19,6 +19,40 @@ function compChoice(){
     return choices[randomNum]; 
 }
 
+// convert words
+function convertWord(letter){
+    if (letter === "roc") return "Rock"; 
+    if (letter === "pape") return "Paper"; 
+    return "Scissors"; 
+}
+
+// game results
+function win(userChoice, computerChoices){
+    userScore++;
+    userScoreEl.innerHTML = userScore;
+    compScoreEl.innerHTML = compScore;
+    const smallUserWord = "user". fontsize(3).sub();
+    const smallCompWord = "comp". fontsize(3).sub();  
+    resultDiv.innerHTML = `${convertWord(userChoice)} ${smallUserWord}  beats ${convertWord(computerChoices)} ${smallCompWord} , You Win!`; 
+}
+
+function lose(){
+    userScore++;
+    userScoreEl.innerHTML = userScore;
+    compScoreEl.innerHTML = compScore;
+    const smallUserWord = "user". fontsize(3).sub();
+    const smallCompWord = "comp". fontsize(3).sub();  
+    resultDiv.innerHTML = `${convertWord(userChoice)} ${smallUserWord}  beats ${convertWord(computerChoices)} ${smallCompWord} , You Win!`; 
+}
+
+function draw(){
+    userScore++;
+    userScoreEl.innerHTML = userScore;
+    compScoreEl.innerHTML = compScore;
+    const smallUserWord = "user". fontsize(3).sub();
+    const smallCompWord = "comp". fontsize(3).sub();  
+    resultDiv.innerHTML = `${convertWord(userChoice)} ${smallUserWord}  beats ${convertWord(computerChoices)} ${smallCompWord} , You Win!`; 
+}
 
 // User
 function game(userChoice){
@@ -28,17 +62,17 @@ function game(userChoice){
         case "rocsciz":
         case "paperoc":
         case "scizpape":
-        console.log("User Wins!!");
+        win(userChoice, computerChoices);
         break;
         case "rocpape":
         case "papesciz":
         case "scizroc":
-        console.log("User Loses!!");
+        lose(userChoice, computerChoices);
         break; 
         case "rocroc":
         case "papepape":
         case "scizsciz":
-        console.log("Its a draw!!");
+        draw(userChoice, computerChoices);
         break; 
     }
 }
